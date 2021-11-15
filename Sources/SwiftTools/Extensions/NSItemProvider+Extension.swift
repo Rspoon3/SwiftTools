e1f88e72070b1d2e38bad148c738c3597450f1bd
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-11-14 22:37:49 -0500

    • Updated NSItemProvider+Extension

--- Sources/SwiftTools/Extensions/NSItemProvider+Extension.swift
+++ Sources/SwiftTools/Extensions/NSItemProvider+Extension.swift
@@ -8,15 +8,30 @@
 import Foundation
 
 
+@available(iOS 15.0.0, *)
 public extension NSItemProvider{
-    
-    @available(iOS 15.0.0, *)
-    func loadObject<T: NSItemProviderReading>(ofClass type: T.Type) async throws -> T{
+    func loadObject<T>(ofClass aClass: T.Type) async throws -> T where T : _ObjectiveCBridgeable, T._ObjectiveCType : NSItemProviderReading{
         return try await withCheckedThrowingContinuation { continuation in
-            loadObject(ofClass: type) { (nsItemProviderReading, error) in
+            _ = loadObject(ofClass: aClass) { (nsItemProviderReading, error) in
                 if let error = error{
                     continuation.resume(throwing: error)
-                } else if let type = nsItemProviderReading as? T {
+                } else if let type = nsItemProviderReading {
+                    continuation.resume(returning: type)
+                } else {
+                    let error = NSError(domain: "Unknown NSItemProviderReading Error", code: -1, userInfo: [:])
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
+    
+    func loadObject<T: NSItemProviderReading>(ofClass aClass: T.Type) async throws -> T {
+        return try await withCheckedThrowingContinuation { continuation in
+            _ = loadObject(ofClass: aClass) { (nsItemProviderReading, error) in
+                if let error = error{
+                    continuation.resume(throwing: error)
+                } else if let type = nsItemProviderReading as? T{
                     continuation.resume(returning: type)
                 } else {
                     let error = NSError(domain: "Unknown NSItemProviderReading Error", code: -1, userInfo: [:])
