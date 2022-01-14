91a7e850fc8c5d54014f8262b50877525e15b686
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2022-01-14 10:14:18 -0500

    Now using the released version of the SFSymbols library

--- Package.swift
+++ Package.swift
@@ -18,9 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        
-        .package(name: "SFSymbols", url: "https://github.com/Rspoon3/SFSymbols.git", branch: "release-2.0.0"),
-        
+        .package(name: "SFSymbols", url: "https://github.com/Rspoon3/SFSymbols.git", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
